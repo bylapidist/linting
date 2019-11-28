fc6f72343c1b88e84936029c85468ab6b73b22ca
Brett Dorrans <hello@lapidist.net>
2019-11-28 21:00:24 +0000

    init

---
+++ src/eslint.js
@@ -0,0 +1,47 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2018,
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+    globals: {
+        document: false,
+        navigator: false,
+        window: false
+    },
+    settings: {
+        react: {
+            pragma: 'React',
+            version: 'detect'
+        }
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/react'
+    ],
+    plugins: ['@typescript-eslint', 'import', 'prettier', 'node', 'promise', 'react', 'react-hooks'],
+    rules: {
+        'prettier/prettier': ['error', { singleQuote: true, semi: true }],
+        'react-hooks/rules-of-hooks': 'error',
+        'react/prop-types': 'off',
+        'react/jsx-filename-extension': [
+            'warn',
+            {
+                extensions: ['.jsx', '.tsx']
+            }
+        ]
+    }
+};
---
+++ src/prettier.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 80,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    semi: true,
+    requirePragma: false,
+    proseWrap: 'preserve',
+    arrowParens: 'always'
+};
