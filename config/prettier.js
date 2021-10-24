01d285355ac2f5116d224767a92561e0594e359d
Brett Dorrans <hello@lapidist.net>
2021-10-24 21:30:36 +0100

    feat: use @stylelint/postcss-css-in-js custom syntax for stylelint (#347)
    * fix: remove deprecated prettier option
    
    * feat: use @stylelint/postcss-css-in-js custom syntax for stylelint

--- config/prettier.js
+++ config/prettier.js
@@ -3,7 +3,6 @@ module.exports = {
     singleQuote: true,
     trailingComma: 'none',
     bracketSpacing: true,
-    jsxBracketSameLine: false,
     semi: true,
     requirePragma: false,
     proseWrap: 'preserve',
