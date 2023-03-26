1583e325a8f373e937e835f98c31fbc4e4536d01
Brett Dorrans <hello@lapidist.net>
2023-03-26 16:36:52 +0100

    fix: update custom-property-pattern regex

--- config/stylelint.js
+++ config/stylelint.js
@@ -4,5 +4,13 @@ module.exports = {
         'stylelint-config-recommended',
         'stylelint-config-css-modules',
         'stylelint-config-standard-scss'
-    ]
+    ],
+    rules: {
+        'custom-property-pattern': [
+            '^([a-z][a-z0-9]*)(-[a-z0-9]+)*(__[a-z0-9]+)*$',
+            {
+                message: 'Expected custom property name to be BEM-like'
+            }
+        ]
+    }
 };
