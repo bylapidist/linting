e30b9bc280bd1edbbfbf1d355480daf8604fd486
Brett Dorrans <hello@lapidist.net>
2023-03-28 22:35:36 +0100

    fix: custom-property-pattern regex matches bem-like__bem-like

--- config/stylelint.js
+++ config/stylelint.js
@@ -7,7 +7,7 @@ module.exports = {
     ],
     rules: {
         'custom-property-pattern': [
-            '^([a-z][a-z0-9]*)(-[a-z0-9]+)*(__[a-z0-9]+)*$',
+            '^([a-z][a-z0-9]*)(-[a-z0-9]+)*((__[a-z0-9]+)*(-[a-z0-9]+)*)$',
             {
                 message: 'Expected custom property name to be BEM-like'
             }
