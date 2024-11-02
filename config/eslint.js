14dd2561a6f3a62b3ed2e228c07398d06cc2c89b
Brett Dorrans <hello@lapidist.net>
2024-11-02 22:30:30 +0000

    feat: eslint 5 support (#717)
    BREAKING CHANGE: flat file config for eslint
    
    * feat: upgrade eslint to v5

--- config/eslint.js
+++ config/eslint.js
@@ -1,75 +1,67 @@
-module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2018,
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
-        'jest/globals': true
-    },
-    globals: {
-        document: false,
-        navigator: false,
-        window: false,
-        jsx: true
-    },
-    settings: {
-        react: {
-            pragma: 'React',
-            version: 'detect'
-        },
-        'mdx/code-blocks': true
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'prettier',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-        'plugin:jest/recommended',
-        'plugin:compat/recommended'
-    ],
-    plugins: [
-        '@typescript-eslint',
-        'import',
-        'prettier',
-        'n',
-        'promise',
-        'react',
-        'react-hooks',
-        'jest',
-        'compat'
-    ],
-    rules: {
-        'prettier/prettier': ['error', { singleQuote: true, semi: true }],
-        'react-hooks/rules-of-hooks': 'error',
-        'react/prop-types': 'off',
-        'react/jsx-filename-extension': [
-            'warn',
-            {
-                extensions: ['.jsx', '.tsx', '.mdx']
-            }
-        ],
-        'compat/compat': 'error'
-    },
-    overrides: [
-        {
-            files: ['*rc.js', '*.config.js', '*.cjs'],
-            rules: {
-                '@typescript-eslint/no-var-requires': 'off',
-                '@typescript-eslint/explicit-function-return-type': 'off'
+const typescriptParser = require('@typescript-eslint/parser');
+const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
+const eslintPluginReact = require('eslint-plugin-react');
+const eslintPluginReactHooks = require('eslint-plugin-react-hooks');
+const eslintPluginJest = require('eslint-plugin-jest');
+const eslintPluginImport = require('eslint-plugin-import');
+const eslintPluginPrettier = require('eslint-plugin-prettier');
+const eslintPluginCompat = require('eslint-plugin-compat');
+const eslintPluginPromise = require('eslint-plugin-promise');
+const eslintPluginNode = require('eslint-plugin-n');
+const prettierConfig = require('./prettier');
+
+const eslintConfig = [
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            parser: typescriptParser,
+            ecmaVersion: 2021,
+            sourceType: 'module',
+            globals: {
+                document: false,
+                navigator: false,
+                window: false,
+                jsx: true
             }
         },
-        {
-            files: ['*.mdx', '*.md'],
-            parser: 'eslint-mdx',
-            extends: 'plugin:mdx/recommended'
+        settings: {
+            react: {
+                pragma: 'React',
+                version: 'detect'
+            },
+            'mdx/code-blocks': true
+        },
+        plugins: {
+            '@typescript-eslint': typescriptPlugin,
+            import: eslintPluginImport,
+            prettier: eslintPluginPrettier,
+            n: eslintPluginNode,
+            promise: eslintPluginPromise,
+            react: eslintPluginReact,
+            'react-hooks': eslintPluginReactHooks,
+            jest: eslintPluginJest,
+            compat: eslintPluginCompat
+        },
+        rules: {
+            'prettier/prettier': ['error', prettierConfig],
+            'react-hooks/rules-of-hooks': 'error',
+            'react/prop-types': 'off',
+            'react/jsx-filename-extension': [
+                'warn',
+                {
+                    extensions: ['.jsx', '.tsx', '.mdx']
+                }
+            ],
+            'compat/compat': 'error'
         }
-    ]
-};
+    },
+    {
+        files: ['**/*rc.js', '**/*.config.js', '**/*.cjs'],
+        rules: {
+            '@typescript-eslint/no-var-requires': 'off',
+            '@typescript-eslint/explicit-function-return-type': 'off'
+        }
+    }
+];
+
+module.exports = eslintConfig;
